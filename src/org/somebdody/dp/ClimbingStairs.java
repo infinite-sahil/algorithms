d480da004a1f276bec74ac32d6af6ebc51329cd3
infinite-sahil <verma.rishu294@gmail.com>
2019-01-30 22:52:09 +0530

    added comments

--- src/org/somebdody/dp/ClimbingStairs.java
+++ src/org/somebdody/dp/ClimbingStairs.java
@@ -20,9 +20,9 @@ public class ClimbingStairs {
 
     public static long climbStairs(final int numberOfStairs) {
         long[] ways = new long[numberOfStairs + 1];
-        // to climb 1 stair, it gonna take 1 step
+        // to climb 1 stair: it gonna take 1 step
         ways[1] = 1;
-        // to 2 stair, it can be (1-climb ,1-climb or direct 2 climb), so 2 ways
+        // to climb 2 stair: it can be done (1-climb ,1-climb OR direct 2 climb), so 2 ways
         ways[2] = 2;
         for (int i = 3; i <= numberOfStairs; i++)
             // here we are getting values from the already derived values (i.e that are present in the ways array)
